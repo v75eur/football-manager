f1607644aa94953f9e1d26b8a6f21cb91f6bb380
v75eur <votre-email@github.com>
2026-08-16 11:04:10 +0000

    Version complète du projet FMTK V1

--- app.py
+++ app.py
@@ -5,22 +5,11 @@ app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
-    return send_file('acceuil/acceuil.html')
+    return send_file('accueil/index.html')
 
-@app.route('/acceuil/<path:path>')
-def acceuil_files(path):
-    return send_from_directory('acceuil', path)
-
-# Route pour les autres fichiers HTML que tu ajouteras
-@app.route('/<path:path>')
-def serve_static(path):
-    if os.path.exists(path):
-        return send_file(path)
-    return "Fichier non trouvé", 404
+@app.route('/accueil/<path:path>')
+def accueil_files(path):
+    return send_from_directory('accueil', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-@app.route('/session/charger')
-def session_charger():
-    return send_file('session/charger.html')
