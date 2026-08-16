9515fdba71234e8ac71aebe7bb08b06ce85bac13
v75eur <votre-email@github.com>
2026-08-16 09:51:36 +0000

    Ajout: page charger session + page jeu (paysage verrouillé)

--- app.py
+++ app.py
@@ -5,11 +5,22 @@ app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
-    return send_file('accueil/index.html')
+    return send_file('acceuil/acceuil.html')
 
-@app.route('/accueil/<path:path>')
-def accueil_files(path):
-    return send_from_directory('accueil', path)
+@app.route('/acceuil/<path:path>')
+def acceuil_files(path):
+    return send_from_directory('acceuil', path)
+
+# Route pour les autres fichiers HTML que tu ajouteras
+@app.route('/<path:path>')
+def serve_static(path):
+    if os.path.exists(path):
+        return send_file(path)
+    return "Fichier non trouvé", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+@app.route('/session/charger')
+def session_charger():
+    return send_file('session/charger.html')
